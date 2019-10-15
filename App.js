0a541258abdba6c6603505d6527a1e0afc304af0
thorgas <timhorgas@gmail.com>
2019-10-15 22:49:05 +0200

    changed import

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Icon } from 'expo';
+//import * as { Font } from 'expo-font';
 import {Asset} from 'expo-asset';
 import AppNavigator from './navigation/AppNavigator';
 
